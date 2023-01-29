2d830f5d2bb10d5a792567b2e3352c92ede6cb76
Georgiy Kozhevnikov <Baraltiva@gmail.com>
2023-01-29 13:17:15 +0300

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,25 +7,14 @@ until poetry update || [[ $counter -gt 2 ]]; do
 done
 poetry run make format
 
-commit_message="Initial project structure based on https://github.com/andredias/perfect_python_project/tree/master"
+commit_message="Initial project structure based on https://github.com/Barahlush/python_project_starter/tree/master"
 
-if [ "{{cookiecutter.version_control}}" == "hg" ]; then
-    hg init .
-    if [ -n "{{cookiecutter.github_respository_url}}" ]; then
-        echo '[paths]
-default = {{cookiecutter.github_respository_url}}
-' > .hg/hgrc
-    fi
-    hg add
-    hg commit -m "$commit_message"
-else
-    mv .hgignore .gitignore
-    git init .
-    if [ -n "{{cookiecutter.github_respository_url}}" ]; then
-        git remote add origin {{cookiecutter.github_respository_url}}
-    fi
-    git add -A .
-    git commit -m "$commit_message"
+mv .hgignore .gitignore
+git init .
+if [ -n "{{cookiecutter.github_respository_url}}" ]; then
+    git remote add origin {{cookiecutter.github_respository_url}}
 fi
+git add -A .
+git commit -m "$commit_message"
 
 make install_hooks
